3fe39fee59efb6dcfc9d4c7196f72a55aa311043
Stas Makarov <stas.makarov.90@gmail.com>
2025-04-25 05:54:04 +0400

    readme etc

--- env/dev/clj/user.clj
+++ env/dev/clj/user.clj
@@ -1,17 +1,19 @@
 (ns user
   "Userspace functions you can run by default in your local REPL."
   (:require
-    [clojure.pprint]
-    [clojure.spec.alpha :as s]
-    [clojure.tools.namespace.repl :as repl]
-    [criterium.core :as c]                                  ;; benchmarking
-    [expound.alpha :as expound]
-    [integrant.core :as ig]
-    [integrant.repl :refer [clear go halt prep init reset reset-all]]
-    [integrant.repl.state :as state]
-    [kit.api :as kit]
-    [lambdaisland.classpath.watch-deps :as watch-deps]      ;; hot loading for deps
-    [jehaby.wpt.core :refer [start-app]]))
+   [clojure.pprint]
+   [clojure.spec.alpha :as s]
+   [clojure.tools.namespace.repl :as repl]
+   [criterium.core :as c]                                  ;; benchmarking
+   [expound.alpha :as expound]
+   [migratus.core :as migratus]
+   [integrant.core :as ig]
+   [integrant.repl :refer [clear go halt prep init reset reset-all]]
+   [integrant.repl.state :as state]
+   [kit.api :as kit]
+   [lambdaisland.classpath.watch-deps :as watch-deps]      ;; hot loading for deps
+   [jehaby.wpt.core :refer [start-app]]
+   [jehaby.wpt.db :as db]))
 
 ;; uncomment to enable hot loading for deps
 (watch-deps/start! {:aliases [:dev :test]})
@@ -19,6 +21,9 @@
 (alter-var-root #'s/*explain-out* (constantly expound/printer))
 
 (add-tap (bound-fn* clojure.pprint/pprint))
+
+(defn migrate []
+  (migratus/init (db/->migratus-cfg state/system)))
 
 (defn dev-prep!
   []
@@ -41,8 +46,14 @@
 
 (def refresh repl/refresh)
 
-
+(defn run []
+  (go)
+  (migrate))
 
 (comment
   (go)
-  (reset))
+  (migrate)
+  (reset)
+  (halt)
+  ;
+  )
